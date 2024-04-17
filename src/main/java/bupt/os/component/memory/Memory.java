9eabc9395961a2c7fee7c1d76bd59ebcfaae9625
buptLiYangCode <1642612105@qq.com>
2024-04-17 11:38:36 +0800

    内存模拟

--- src/main/java/bupt/os/component/memory/Memory.java
+++ src/main/java/bupt/os/component/memory/Memory.java
@@ -1,6 +1,16 @@
 package bupt.os.component.memory;
 
+import lombok.Data;
+import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
+/**
+ * 内存
+ * 当页不足时，有对应的置换算法，所以不需要位图
+ */
+@Component
+@Data
 public class Memory {
     // 内存大小，例如16KB
     public static final int MEMORY_SIZE = 16 * 1024;
@@ -8,6 +18,9 @@ public class Memory {
     public static final int TOTAL_PAGES = 4;
     // 页大小，例如4KB
     public static final int PAGE_SIZE = 4 * 1024;
+
+    // 二维数组，行坐标表示物理页号，列（调入内存时间、最近访问时间、未来访问时间）
+    private LocalDateTime[][] pageAccessTime;
     // 页数组
-    public static Page[] pages;
+    private Page[] pages;
 }
