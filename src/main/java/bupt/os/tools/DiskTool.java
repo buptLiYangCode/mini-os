f73174040857bc8809a7cd1be90d18ea65955ff1
buptLiYangCode <1642612105@qq.com>
2024-04-17 11:40:38 +0800

    根据哈希值获取inode号

--- src/main/java/bupt/os/tools/DiskTool.java
+++ src/main/java/bupt/os/tools/DiskTool.java
@@ -9,7 +9,7 @@ import java.util.Random;
 public class DiskTool {
 
     // 根据文件、目录名计算哈希值 % iNodeBitmap大小，得出文件、目录的inode号
-    public static int getFreeINode(Disk disk, String fileName) {
+    public static int getINodeIndex(Disk disk, String fileName) {
         boolean[] iNodeBitmap = disk.getINodeBitmap();
         int hashCode = fileName.hashCode(); // 获取文件名的哈希值
         int index = Math.abs(hashCode) % iNodeBitmap.length; // 取模得出索引
