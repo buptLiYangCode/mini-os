7098a3eeb759ec8661420352656a907540b9ff28
buptLiYangCode <1642612105@qq.com>
2024-04-17 11:39:11 +0800

    PCB字段更新

--- src/main/java/bupt/os/component/process/PCB.java
+++ src/main/java/bupt/os/component/process/PCB.java
@@ -1,31 +1,37 @@
 package bupt.os.component.process;
 
-import bupt.os.common.constant.ProcessStateConstant;
 import bupt.os.component.filesystem.INode;
 import bupt.os.component.filesystem.OpenFileEntry;
 import bupt.os.component.memory.PageTableEntry;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class PCB {
     // 进程id
     private int pid;
+    // 进程名
+    private String processName;
     // 指令寄存器，保存当前正在执行指令地址
     private int ir;
     // 进程大小，单位为B
     private int size;
     // 进程状态
-    private ProcessStateConstant state;
+    private String state;
     // 工作目录,进程当前所在的目录，影响着进程对文件的操作
     private INode iNode;
 
     // 文件描述符表
     private HashMap<Integer, OpenFileEntry> fileDescriptorTable;
     // 页表，VPN -> PPN
-    private HashMap<Integer, PageTableEntry> pageTable;
+    private LinkedList<PageTableEntry> pageTable;
 
 }
