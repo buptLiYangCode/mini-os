2b243e0e831f9555eb88966a1449ce868fa5de75
buptLiYangCode <1642612105@qq.com>
2024-04-18 21:15:33 +0800

    任务对象属性是processName

--- src/main/java/bupt/os/component/cpu/ProcessExecutionTask.java
+++ src/main/java/bupt/os/component/cpu/ProcessExecutionTask.java
@@ -1,6 +1,17 @@
 package bupt.os.component.cpu;
 
+import bupt.os.component.interrupt.InterruptRequestLine;
+import bupt.os.component.memory.PCB;
+import bupt.os.component.memory.ProtectedMemory;
+import bupt.os.tools.DiskTool;
+
+import java.util.HashMap;
+import java.util.Queue;
+
+import static bupt.os.component.interrupt.InterruptHandler.handleInterrupt;
+
 public class ProcessExecutionTask implements Runnable {
+
     private final String processName;
     private final String[] instructions;
 
@@ -13,15 +24,36 @@ public class ProcessExecutionTask implements Runnable {
     public void run() {
         for (String instruction : instructions) {
             executeInstruction(instruction);
+            // CPU每执行一条指令，都需要去检查 irl 是否有信号
+            InterruptRequestLine interruptRequestLine = InterruptRequestLine.getInstance();
+            String interruptRequest = interruptRequestLine.get();
+            if (interruptRequest != null) {
+                System.out.println("收到中断信号: " + interruptRequest);
+                // 处理中断信号
+                handleInterrupt(interruptRequest);
+            }
         }
     }
+
 
     private void executeInstruction(String instruction) {
         String[] parts = instruction.split(" ");
         String command = parts[0];
-
         try {
             switch (command) {
+                case "S" -> {
+                    // 进程（任务）开始执行时，将进程pcb放进运行队列
+                    int pid = DiskTool.getINodeIndex(processName) - 1;
+                    ProtectedMemory protectedMemory = ProtectedMemory.getInstance();
+                    HashMap<Integer, PCB> pcbTable = protectedMemory.getPcbTable();
+                    PCB pcb = pcbTable.get(pid);
+                    // 运行队列
+                    Queue<PCB> runningQueue = protectedMemory.getRunningQueue();
+                    runningQueue.add(pcb);
+                    // 就绪队列
+                    Queue<PCB> readyQueue = protectedMemory.getReadyQueue();
+                    readyQueue.remove(pcb);
+                }
                 case "C" -> {
                     int computeTime = Integer.parseInt(parts[1]);
                     System.out.println("Computing for " + computeTime + " units of time.");
