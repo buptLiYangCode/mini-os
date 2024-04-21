c556fb2f1932c867df3409921f462bf58f3654a2
buptLiYangCode <1642612105@qq.com>
2024-04-22 00:23:16 +0800

    删除无用属性

--- src/main/java/bupt/os/component/memory/PageInfo.java
+++ src/main/java/bupt/os/component/memory/PageInfo.java
@@ -15,8 +15,4 @@ public class PageInfo {
     private int pageNumber;
     // 页是否在物理内存中
     private boolean present;
-    // 页是否被修改过
-    private boolean dirty;
-    // 页是否被访问过，用于某些页面替换算法
-    private boolean referenced;
 }
