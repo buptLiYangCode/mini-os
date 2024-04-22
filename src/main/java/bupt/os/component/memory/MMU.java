3ec52a2162bfedda90dee663ca59b46021217182
buptLiYangCode <1642612105@qq.com>
2024-04-23 00:51:17 +0800

    获取可用页，更新两张页表

--- src/main/java/bupt/os/component/memory/MMU.java
+++ src/main/java/bupt/os/component/memory/MMU.java
@@ -2,12 +2,7 @@ package bupt.os.component.memory;
 
 import bupt.os.component.disk.Disk;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-
-import static bupt.os.component.disk.Disk.BLOCK_SIZE;
+import java.util.*;
 
 /**
  * 内存管理单元：不存在物理实体，也就没有实例对象，方法全定义成静态。不必采用单例模式
@@ -19,6 +14,7 @@ public class MMU {
 
     /**
      * 访问页，如果页不在内存，返回false
+     *
      * @param pcb pcb
      * @param vpn 虚拟页号
      * @return 页是否在内存
@@ -34,37 +30,25 @@ public class MMU {
         return isPageFault;
     }
 
-    /**
-     * 将指定块加载进内存
-     * @param blockNumbers 块号
-     */
-    public static Integer loadPageIntoMemory(LinkedList<Integer> blockNumbers) {
-        // 先使用页置换算法，选出一个可以存放磁盘数据的页
-        Integer availablePageNumber = lruPageSwap(1).get(0);
-        // 写入页
-        char[][] blocks = disk.getBlocks();
-        char[][] pages = userMemory.getPages();
-        for (int i = 0; i < blockNumbers.size(); i++) {
-            System.arraycopy(blocks[i], 0, pages[availablePageNumber], i * BLOCK_SIZE, BLOCK_SIZE);
-        }
-        return availablePageNumber;
-    }
-
 
     /**
      * 采用lru算法获取页号
-     * @param requiredPageCount 需要页的数量
-     * @return 页号集合
+     *
+     * @return 页号
      */
-    public static LinkedList<Integer> lruPageSwap(int requiredPageCount) {
+    public static int lruPageSwap() {
         LinkedList<PageSwapInfo> allPagesInfo = protectedMemory.getAllPagesInfo();
-        // 选出k个最小lastAccessTime 最小的
-        List<Integer> availablePageNumbers = allPagesInfo.stream()
-                .sorted(Comparator.comparingLong(PageSwapInfo::getLastAccessTime))
-                .limit(requiredPageCount)
-                .map(PageSwapInfo::getPpn)
-                .toList();
+        // 选出1个lastAccessTime 最小的页
+        int resNumber = -1;
+        long latestAccessTime = System.currentTimeMillis();
+        for (int i = 0; i < allPagesInfo.size(); i++) {
+            PageSwapInfo pageSwapInfo = allPagesInfo.get(i);
+            if (pageSwapInfo.getLastAccessTime() < latestAccessTime) {
+                latestAccessTime = pageSwapInfo.getLastAccessTime();
+                resNumber = i;
+            }
+        }
 
-        return new LinkedList<>(availablePageNumbers);
+        return resNumber;
     }
 }
