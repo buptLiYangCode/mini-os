6a83b535a695d863452d7a5299c3ae54c45c036b
buptLiYangCode <1642612105@qq.com>
2024-04-24 00:15:41 +0800

    访问页时，需要更新访问时间

--- src/main/java/bupt/os/component/memory/MMU.java
+++ src/main/java/bupt/os/component/memory/MMU.java
@@ -1,16 +1,17 @@
 package bupt.os.component.memory;
 
-import bupt.os.component.disk.Disk;
-
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 /**
  * 内存管理单元：不存在物理实体，也就没有实例对象，方法全定义成静态。不必采用单例模式
  */
 public class MMU {
-    private static final Disk disk = Disk.getInstance();
-    private static final UserMemory userMemory = UserMemory.getInstance();
     private static final ProtectedMemory protectedMemory = ProtectedMemory.getInstance();
+
+    private static final LinkedList<PageSwapInfo> allPagesInfo = protectedMemory.getAllPagesInfo();
+    private static final HashMap<Integer, LinkedList<PageInfo>> processPageTable = protectedMemory.getProcessPageTable();
+
 
     /**
      * 访问页，如果页不在内存，返回false
@@ -21,11 +22,13 @@ public class MMU {
      */
     public static boolean accessPage(PCB pcb, int vpn) {
         boolean isPageFault = false;
-        HashMap<Integer, LinkedList<PageInfo>> processPageTable = protectedMemory.getProcessPageTable();
         PageInfo pageInfo = processPageTable.get(pcb.getPid()).get(vpn);
         if (!pageInfo.isPresent()) {
             System.out.println("进程" + pcb.getProcessName() + "vpn：" + vpn + "ppn：" + pageInfo.getPageNumber() + "不在内存中");
             isPageFault = true;
+        } else {
+            PageSwapInfo pageSwapInfo = allPagesInfo.get(pageInfo.getPageNumber());
+            pageSwapInfo.setLastAccessTime(System.currentTimeMillis());
         }
         return isPageFault;
     }
