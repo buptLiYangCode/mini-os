eee4563fd09cfd0d8a72f6452e0fdf65ab9006ef
buptLiYangCode <1642612105@qq.com>
2024-04-24 00:15:04 +0800

    小修改

--- src/main/java/bupt/os/component/timer/Timer.java
+++ src/main/java/bupt/os/component/timer/Timer.java
@@ -1,7 +1,6 @@
 package bupt.os.component.timer;
 
 import bupt.os.component.interrupt.InterruptRequestLine;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +10,6 @@ public class Timer {
     @Scheduled(fixedRate = 600) // 每隔600ms执行一次
     public void sendInterruptRequest() {
         InterruptRequestLine interruptRequestLine = InterruptRequestLine.getInstance();
-        interruptRequestLine.put("TIMER_INTERRUPT");
+        interruptRequestLine.offer("TIMER_INTERRUPT");
     }
 }
