ce4a5b226c8f93cca7b8d04ad93ab6ecbb0aae02
buptLiYangCode <1642612105@qq.com>
2024-04-23 20:08:59 +0800

    删去无用常量

--- src/main/java/bupt/os/common/constant/ProcessStateConstant.java
+++ src/main/java/bupt/os/common/constant/ProcessStateConstant.java
@@ -5,7 +5,5 @@ public class ProcessStateConstant {
     public static final String READY = "READY";
     public static final String WAITING = "WAITING";
     public static final String RUNNING = "RUNNING";
-    public static final String SUSPENDED_READY = "SUSPENDED_READY";
-    public static final String SUSPENDED_WAITING = "SUSPENDED_WAITING";
     public static final String TERMINATED = "TERMINATED";
 }
