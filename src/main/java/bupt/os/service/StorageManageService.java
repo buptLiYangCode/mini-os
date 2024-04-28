d2531f82d9ca4c2cdbbbfbec7ff9ec2b8f1b241b
buptLiYangCode <1642612105@qq.com>
2024-04-28 13:10:54 +0800

    存储模块和前端交互

--- src/main/java/bupt/os/service/StorageManageService.java
+++ src/main/java/bupt/os/service/StorageManageService.java
@@ -1,4 +1,7 @@
 package bupt.os.service;
 
+import bupt.os.dto.resp.MemoryQueryAllRespDTO;
+
 public interface StorageManageService {
+    MemoryQueryAllRespDTO queryAllMemoryInfo();
 }
--- src/main/java/bupt/os/service/impl/StorageManageServiceImpl.java
+++ src/main/java/bupt/os/service/impl/StorageManageServiceImpl.java
@@ -1,12 +1,24 @@
 package bupt.os.service.impl;
 
-import lombok.RequiredArgsConstructor;
+import bupt.os.component.memory.PageSwapInfo;
+import bupt.os.component.memory.ProtectedMemory;
+import bupt.os.component.memory.UserMemory;
+import bupt.os.dto.resp.MemoryQueryAllRespDTO;
+import bupt.os.service.StorageManageService;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedList;
+
 @Service
-@RequiredArgsConstructor
-public class StorageManageServiceImpl {
+public class StorageManageServiceImpl implements StorageManageService {
 
+    private final UserMemory userMemory = UserMemory.getInstance();
+    private final ProtectedMemory protectedMemory = ProtectedMemory.getInstance();
 
+    @Override
+    public MemoryQueryAllRespDTO queryAllMemoryInfo() {
 
+        LinkedList<PageSwapInfo> pageSwapInfoList = protectedMemory.getAllPageInfo();
+        return new MemoryQueryAllRespDTO(pageSwapInfoList);
+    }
 }
